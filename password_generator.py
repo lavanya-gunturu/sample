5664a74ce6d6ba391a3d5a4c1fec3b5990e67169
Lavanya Gunturu <gunturu.lavanya@gmail.com>
2021-07-22 17:41:00 -0400

    defining the variables for password characters

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+import string
+import random
+
+#define the uppercase alphabets
+StrUpperCase = string.ascii_uppercase
+
+#define the lowercase alphabets
+StrLowerCase = string.ascii_lowercase
+
+#define the numbers or digits
+Numb = string.digits
+
+#define the special characters
+Char = string.punctuation
