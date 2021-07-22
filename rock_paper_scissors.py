7ac7443392ee4023c58632982695cf0db155979f
Lavanya Gunturu <gunturu.lavanya@gmail.com>
2021-07-22 18:09:22 -0400

    define the rules for the game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,4 +4,14 @@ def play():
     user = input(" 'r' for rock, 'p' for paper, 's' for scissors")
     computer = random.choice(['r', 'p', 's'])
 
+if user == computer:
+    return 'tie'
+
+#r>s, s>p, p>r
+if is_win(user, computer):
+    return 'won';
+
+    if is_win(computer, user):
+        return 'lost';
+
     
