1d98995ca3e35dbbfdc3139eb0863ef43e68eb1d
Lavanya Gunturu <gunturu.lavanya@gmail.com>
2021-07-23 02:53:23 -0400

    decide the winner

--- r_p_s.py
+++ r_p_s.py
@@ -4,3 +4,8 @@ def play():
     user = input("what is your choice? 'r' for rock, 'p' for paper, 's' for scissors\n" )
     computer = random.choice(['r', 'p', 's'])
 
+def is_win(player, opponent):
+    #return true if player wins
+    #r>s, s>p, p>r
+    if (player =='r' and opponent =='s') or (player == 's' and opponent == 'p') or (player == 'r' and opponent == 'r'):
+        return True
