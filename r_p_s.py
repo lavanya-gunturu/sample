1af05b4d31572095b0f62a1d3e1d0260044f1e38
Lavanya Gunturu <gunturu.lavanya@gmail.com>
2021-07-23 03:01:24 -0400

    frame the game rules

--- r_p_s.py
+++ r_p_s.py
@@ -4,8 +4,19 @@ def play():
     user = input("what is your choice? 'r' for rock, 'p' for paper, 's' for scissors\n" )
     computer = random.choice(['r', 'p', 's'])
 
+    if user == computer:
+        return 'ITS A TIE'
+
+    #r>s, s>p, p>r
+    if is_win(user, computer):
+        return 'YOU WON!!!'
+
+    return 'YOU LOST :('
+
 def is_win(player, opponent):
     #return true if player wins
     #r>s, s>p, p>r
     if (player =='r' and opponent =='s') or (player == 's' and opponent == 'p') or (player == 'r' and opponent == 'r'):
         return True
+
+print(play())
