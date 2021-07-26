2de09c3182d6a2e694b4f2f4933b0a35823ede18
Lavanya Gunturu <gunturu.lavanya@gmail.com>
2021-07-26 08:06:57 -0400

    import functions and establish mysql connector

---
+++ register_form.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+import random
+import tkinter.ttk
+
+#import mysql connector
+import mysql.connector 
+db_connection = mysql.connector.connect(
+    host-"localhost",
+    user="root",
+    password=""
+)
