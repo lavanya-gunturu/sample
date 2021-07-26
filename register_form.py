7c15f2ddb3252749f8e6fe268ab8a32607c610a2
Lavanya Gunturu <gunturu.lavanya@gmail.com>
2021-07-26 08:24:56 -0400

    define the elements of GUI

--- register_form.py
+++ register_form.py
@@ -18,3 +18,10 @@ class LoginApp(tk.Tk):
      self.title("Login")
      self.geometry("600*450+351+174")
      self.configure(bg="#000000")
+     #define the elements of GUI
+     self.lblHeading = tk.Label(self,text="welcome to login psge", font=("Helvetica", 16),bg="black",fg="white")
+     self.lbluname = tk.Label(self,tect="Enter Username:", font=("Helvetica", 10),bg="black",fg="white")
+     self.lblpasswd = tk.Label(self,text="Enter Password:", font=("Helvetica", 11),bg="black",fg="white")
+     self.txtuname = tk.Entry(self,width=60)
+     self.txtpasswd = tk.Entry(self,width=60, show="*")
+     
